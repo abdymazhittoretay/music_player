ce0f9eaee22b2821cd5756fd33334d00a142d9fd
Toretay <abdymvzhit@gmail.com>
2025-01-18 16:33:15 +0500

    added playlist and songIndex using consumer in s_p

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/models/song_model.dart';
+import 'package:music_player/providers/playlist_provider.dart';
+import 'package:provider/provider.dart';
 
 class SongPage extends StatefulWidget {
   const SongPage({super.key});
@@ -10,15 +13,22 @@ class SongPage extends StatefulWidget {
 class _SongPageState extends State<SongPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.grey[300],
-        foregroundColor: Colors.black,
-        surfaceTintColor: Colors.grey[300],
-        title: Text("P L A Y L I S T"),
-        centerTitle: true,
-      ),
+    return Consumer<PlaylistProvider>(
+      builder: (context, value, child) {
+        final List<SongModel> playlist = value.playlist;
+
+        final int? selectedSong = value.selectedSong;
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.grey[300],
+            foregroundColor: Colors.black,
+            surfaceTintColor: Colors.grey[300],
+            title: Text("P L A Y L I S T"),
+            centerTitle: true,
+          ),
+        );
+      },
     );
   }
 }
