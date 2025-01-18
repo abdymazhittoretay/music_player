2dc50c21aa689d45f360aa6f90dd71332a8a2346
Toretay <abdymvzhit@gmail.com>
2025-01-18 16:56:21 +0500

    added song info in song_page

--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -17,7 +17,7 @@ class _SongPageState extends State<SongPage> {
       builder: (context, value, child) {
         final List<SongModel> playlist = value.playlist;
 
-        final int? selectedSong = value.selectedSong;
+        final SongModel selectedSong = playlist[value.selectedSong ?? 0];
         return Scaffold(
           backgroundColor: Colors.white,
           appBar: AppBar(
@@ -26,6 +26,60 @@ class _SongPageState extends State<SongPage> {
             surfaceTintColor: Colors.grey[300],
             title: Text("P L A Y L I S T"),
             centerTitle: true,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    padding: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image.asset(
+                            selectedSong.imagePath,
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    selectedSong.songName,
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    selectedSong.artistName,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+              ],
+            ),
           ),
         );
       },
